e4b5c17973e3f8145bb4c1fc9e433cfc1230c75a
mayankdhiman27 <mayankdhiman27@gmail.com>
2019-06-22 22:59:15 +0530

    adding repository

---
+++ SpringBackendServiceApp/src/main/java/com/mayank/SpringBackendApp/SpringBackendServiceApp/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mayank.SpringBackendApp.SpringBackendServiceApp.repository;
+
+import com.mayank.SpringBackendApp.SpringBackendServiceApp.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
