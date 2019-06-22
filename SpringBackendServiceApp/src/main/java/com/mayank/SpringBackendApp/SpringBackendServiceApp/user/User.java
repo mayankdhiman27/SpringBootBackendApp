860e5bbd5a842744631c6c3a77dbfba5eda5b095
Dhiman Mayank <mayankdhiman27@gmail.com>
2019-06-22 23:56:52 +0530

    Update User.java

--- SpringBackendServiceApp/src/main/java/com/mayank/SpringBackendApp/SpringBackendServiceApp/user/User.java
+++ SpringBackendServiceApp/src/main/java/com/mayank/SpringBackendApp/SpringBackendServiceApp/user/User.java
@@ -1,13 +1,10 @@
 package com.mayank.SpringBackendApp.SpringBackendServiceApp.user;
 
-import lombok.extern.slf4j.Slf4j;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity @Slf4j
 public class User {
 
     public User(){
