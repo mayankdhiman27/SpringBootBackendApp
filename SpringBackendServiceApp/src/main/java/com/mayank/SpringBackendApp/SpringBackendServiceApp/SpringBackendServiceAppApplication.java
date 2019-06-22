51c08d1f9d9157164d3adf91d9bc98171b56bb8e
mayankdhiman27 <mayankdhiman27@gmail.com>
2019-06-22 22:51:41 +0530

    adding files

---
+++ SpringBackendServiceApp/src/main/java/com/mayank/SpringBackendApp/SpringBackendServiceApp/SpringBackendServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.SpringBackendApp.SpringBackendServiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendServiceAppApplication.class, args);
+	}
+
+}
---
+++ SpringBackendServiceApp/src/test/java/com/mayank/SpringBackendApp/SpringBackendServiceApp/SpringBackendServiceAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mayank.SpringBackendApp.SpringBackendServiceApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBackendServiceAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
